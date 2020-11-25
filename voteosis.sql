6100fc4b868ef88550fed365e8754e82c95a5093
Muhammad Alif Nurfauzi <56574544+aliffauzi34@users.noreply.github.com>
2020-11-26 05:52:21 +0700

    Update voteosis.sql

--- voteosis.sql
+++ voteosis.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 18 Okt 2019 pada 11.39
--- Versi server: 10.4.6-MariaDB
--- Versi PHP: 7.3.9
+-- Generation Time: Nov 25, 2020 at 11:51 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `t_admin`
+-- Table structure for table `t_admin`
 --
 
 CREATE TABLE `t_admin` (
@@ -36,16 +35,16 @@ CREATE TABLE `t_admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `t_admin`
+-- Dumping data for table `t_admin`
 --
 
 INSERT INTO `t_admin` (`id_admin`, `username`, `fullname`, `password`) VALUES
-(1, 'admin', 'Administrator', '$2y$10$5ok3rcIOv/yNIlPIGo49a.cXRAiA5ZsnxbpijFoyy5EuuYyVrZetu');
+(1, 'osis.kanebsa', 'Administrator', '$2y$10$7cn0v7KHLVvfg4UKNSbVs.nM47YX8oAIbF.G.zTsVv3bqAgHI3DEe');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `t_kandidat`
+-- Table structure for table `t_kandidat`
 --
 
 CREATE TABLE `t_kandidat` (
@@ -53,23 +52,15 @@ CREATE TABLE `t_kandidat` (
   `nama_calon` varchar(50) NOT NULL,
   `foto` varchar(255) NOT NULL,
   `visi` varchar(255) NOT NULL,
-  `misi` varchar(255) NOT NULL,
-  `suara` smallint(4) NOT NULL DEFAULT 0,
+  `misi` varchar(1000) NOT NULL,
+  `suara` int(4) NOT NULL,
   `periode` varchar(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `t_kandidat`
---
-
-INSERT INTO `t_kandidat` (`id_kandidat`, `nama_calon`, `foto`, `visi`, `misi`, `suara`, `periode`) VALUES
-(9, 'Kandidat 1', '0.17692300 1571381327.png', 'Maju', 'Maju', 3, '2019/2020'),
-(10, 'Kandidat 2', '0.05067500 1571381344.png', 'Terus ', 'Terus', 2, '2019/2020');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `t_kelas`
+-- Table structure for table `t_kelas`
 --
 
 CREATE TABLE `t_kelas` (
@@ -78,7 +69,7 @@ CREATE TABLE `t_kelas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `t_kelas`
+-- Dumping data for table `t_kelas`
 --
 
 INSERT INTO `t_kelas` (`id_kelas`, `nama_kelas`) VALUES
@@ -98,7 +89,7 @@ INSERT INTO `t_kelas` (`id_kelas`, `nama_kelas`) VALUES
 ('K14', 'X - AV 2'),
 ('K15', 'XI - AV 1'),
 ('K16', 'XI - AV 2'),
-('K17', 'XII - AV 2'),
+('K17', 'XII - AV 1'),
 ('K18', 'XII - AV 2'),
 ('K19', 'X - LAS 1'),
 ('K20', 'X - LAS 2'),
@@ -143,130 +134,86 @@ INSERT INTO `t_kelas` (`id_kelas`, `nama_kelas`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `t_pemilih`
+-- Table structure for table `t_pemilih`
 --
 
 CREATE TABLE `t_pemilih` (
   `id` int(11) NOT NULL,
   `nis` varchar(25) NOT NULL,
-  `periode` varchar(25) NOT NULL
+  `periode` varchar(25) NOT NULL,
+  `status` enum('0','1') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `t_user`
+-- Table structure for table `t_user`
 --
 
 CREATE TABLE `t_user` (
-  `id_user` varchar(10) NOT NULL,
+  `id_user` varchar(15) NOT NULL,
   `fullname` varchar(50) NOT NULL,
   `id_kelas` varchar(3) NOT NULL,
   `jk` enum('L','P') NOT NULL,
-  `pemilih` enum('Y','N') NOT NULL DEFAULT 'Y'
+  `pemilih` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `status` enum('0','1') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `t_user`
---
-
-INSERT INTO `t_user` (`id_user`, `fullname`, `id_kelas`, `jk`, `pemilih`) VALUES
-('1', 'a', 'K02', 'L', 'Y'),
-('10', 'j', 'K01', 'P', 'Y'),
-('11', 'k', 'K01', 'P', 'Y'),
-('12', 'l', 'K01', 'P', 'Y'),
-('13', 'm', 'K01', 'P', 'Y'),
-('14', 'n', 'K01', 'P', 'Y'),
-('15', 'o', 'K01', 'P', 'Y'),
-('16', 'p', 'K01', 'P', 'Y'),
-('17', 'q', 'K01', 'P', 'Y'),
-('18', 'r', 'K01', 'P', 'Y'),
-('19', 's', 'K01', 'P', 'Y'),
-('2', 'b', 'K02', 'L', 'Y'),
-('20', 't', 'K01', 'P', 'Y'),
-('21', 'u', 'K01', 'P', 'Y'),
-('22', 'v', 'K01', 'P', 'Y'),
-('23', 'w', 'K01', 'P', 'Y'),
-('24', 'x', 'K01', 'P', 'Y'),
-('25', 'y', 'K01', 'P', 'Y'),
-('26', 'z', 'K01', 'P', 'Y'),
-('27', 'sfgsg', 'K01', 'P', 'Y'),
-('28', 'fgdfgdfg', 'K01', 'P', 'Y'),
-('29', 'dfgdfg', 'K01', 'P', 'Y'),
-('3', 'c', 'K02', 'L', 'Y'),
-('30', 'dfgdfg', 'K01', 'P', 'Y'),
-('31', 'dfgadf', 'K01', 'P', 'Y'),
-('32', 'qjwyj', 'K01', 'P', 'Y'),
-('33', 'uiwetiu', 'K01', 'P', 'Y'),
-('34', 'ssdf', 'K01', 'P', 'Y'),
-('35', 'fsdf', 'K01', 'P', 'Y'),
-('36', 'ass', 'K01', 'P', 'Y'),
-('37', 'sf', 'K01', 'P', 'Y'),
-('38', 'qwe', 'K01', 'P', 'Y'),
-('39', 'eq', 'K01', 'P', 'Y'),
-('4', 'd', 'K01', 'L', 'Y'),
-('40', 'sdf', 'K01', 'P', 'Y'),
-('5', 'e', 'K02', 'P', 'Y'),
-('6', 'f', 'K02', 'P', 'Y'),
-('7', 'g', 'K02', 'P', 'Y'),
-('8', 'h', 'K01', 'P', 'Y'),
-('9', 'i', 'K01', 'P', 'Y');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `t_admin`
+-- Indexes for table `t_admin`
 --
 ALTER TABLE `t_admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
--- Indeks untuk tabel `t_kandidat`
+-- Indexes for table `t_kandidat`
 --
 ALTER TABLE `t_kandidat`
   ADD PRIMARY KEY (`id_kandidat`);
 
 --
--- Indeks untuk tabel `t_kelas`
+-- Indexes for table `t_kelas`
 --
 ALTER TABLE `t_kelas`
   ADD PRIMARY KEY (`id_kelas`);
 
 --
--- Indeks untuk tabel `t_pemilih`
+-- Indexes for table `t_pemilih`
 --
 ALTER TABLE `t_pemilih`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `t_user`
+-- Indexes for table `t_user`
 --
 ALTER TABLE `t_user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `t_admin`
+-- AUTO_INCREMENT for table `t_admin`
 --
 ALTER TABLE `t_admin`
   MODIFY `id_admin` tinyint(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `t_kandidat`
+-- AUTO_INCREMENT for table `t_kandidat`
 --
 ALTER TABLE `t_kandidat`
-  MODIFY `id_kandidat` smallint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_kandidat` smallint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT untuk tabel `t_pemilih`
+-- AUTO_INCREMENT for table `t_pemilih`
 --
 ALTER TABLE `t_pemilih`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
